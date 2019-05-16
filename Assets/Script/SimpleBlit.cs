d08f57de1c88dd686470771743f4ce16097b2448
Keijiro Takahashi <keijiro@gmail.com>
2019-05-17 07:22:01 +0900

    Add label to SimpleBlit

--- Assets/Script/SimpleBlit.cs
+++ Assets/Script/SimpleBlit.cs
@@ -9,14 +9,25 @@ namespace Evp3
     {
         [SerializeField] RenderTexture _source = null;
 
-        void Start()
+        string _label;
+
+        void OnEnable()
         {
-            GetComponent<HDAdditionalCameraData>().customRender += CustomRender;
+            _label = "Simple Blit :: " + gameObject.name;
+
+            var data = GetComponent<HDAdditionalCameraData>();
+            if (data != null) data.customRender += CustomRender;
+        }
+
+        void OnDisable()
+        {
+            var data = GetComponent<HDAdditionalCameraData>();
+            if (data != null) data.customRender -= CustomRender;
         }
 
         void CustomRender(ScriptableRenderContext context, HDCamera camera)
         {
-            var cmd = CommandBufferPool.Get("");
+            var cmd = CommandBufferPool.Get(_label);
 
             cmd.SetViewport(new Rect(0, 0, Screen.width, Screen.height));
             CoreUtils.ClearRenderTarget(cmd, ClearFlag.All, Color.clear);
